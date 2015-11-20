409e365ad7267ca237fe34c9b54fe680a50a21d6
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2015-11-20 20:26:16 -0200

    Definindo Context e Models

---
+++ src/WebApplication2/Models/MyContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.Entity;
+using System.Collections.Generic;
+
+namespace WebApplication2.Models
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Blog> Blog { get; set; }
+        public DbSet<Post> Post { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Make Blog.Url required
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.Url)
+                .IsRequired();
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
