ee0154685e59282aa050ddeb376e9f917e18ae84
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2015-11-20 21:02:30 -0200

    Arquivos de migração

---
+++ src/WebApplication2/Migrations/20151120222746_MyFirstMigration.Designer.cs
@@ -0,0 +1,53 @@
+using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
+using WebApplication2.Models;
+
+namespace WebApplication2.Migrations
+{
+    [DbContext(typeof(MyContext))]
+    [Migration("20151120222746_MyFirstMigration")]
+    partial class MyFirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Annotation("ProductVersion", "7.0.0-beta8-15964")
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("WebApplication2.Models.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Url")
+                        .IsRequired();
+
+                    b.HasKey("BlogId");
+                });
+
+            modelBuilder.Entity("WebApplication2.Models.Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PostId");
+                });
+
+            modelBuilder.Entity("WebApplication2.Models.Post", b =>
+                {
+                    b.HasOne("WebApplication2.Models.Blog")
+                        .WithMany()
+                        .ForeignKey("BlogId");
+                });
+        }
+    }
+}
