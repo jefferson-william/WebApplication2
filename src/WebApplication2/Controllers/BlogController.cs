f517e92bbc5e00bf0d13325d9065511b1f663d82
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2015-11-20 21:02:44 -0200

    Criando controller do Blog

---
+++ src/WebApplication2/Controllers/BlogController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private MyContext _context;
+
+        public BlogsController(MyContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Blog.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Blog.Add(blog);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blog);
+        }
+
+    }
+}
